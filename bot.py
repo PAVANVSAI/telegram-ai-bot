93780a02b778e402345ce5ebc82a2792973be657
PAVANVSAI <pavanvsaimakkala12@gmail.com>
2026-05-26 19:57:04 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,16 +3,36 @@ from telegram import Bot, Update
 from openai import OpenAI
 import asyncio
 
-BOT_TOKEN = "8943413347:AAH4c5g_arJB3CM-3n1elkiCwU7v0wLmvWM"
-OPENROUTER_API_KEY = "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"
+# =========================
+# YOUR TOKENS
+# =========================
+import os
+
+BOT_TOKEN = os.getenv("8943413347:AAH4c5g_arJB3CM-3n1elkiCwU7v0wLmvWM")
+
+OPENROUTER_API_KEY = os.getenv("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")
+
+
+# =========================
+# OPENROUTER CLIENT
+# =========================
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=OPENROUTER_API_KEY
+    api_key=OPENROUTER_API_KEY,
 )
+
+# =========================
+# TELEGRAM BOT
+# =========================
+
 bot = Bot(token=BOT_TOKEN)
 
 app = Flask(__name__)
+
+# =========================
+# WEBHOOK
+# =========================
 
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def webhook():
@@ -26,9 +46,16 @@ def webhook():
             user_message = update.message.text
 
             completion = client.chat.completions.create(
+                extra_headers={
+                    "HTTP-Referer": "https://telegram-ai-bot-3yur.onrender.com",
+                    "X-Title": "telegram-ai-bot",
+                },
                 model="meta-llama/llama-3-8b-instruct:free",
                 messages=[
-                    {"role": "user", "content": user_message}
+                    {
+                        "role": "user",
+                        "content": user_message
+                    }
                 ]
             )
 
@@ -40,15 +67,24 @@ def webhook():
                     text=reply_text
                 )
             )
+
         return "ok"
 
     except Exception as e:
         print(e)
         return "error"
 
+# =========================
+# HOME ROUTE
+# =========================
+
 @app.route("/")
 def home():
     return "Bot is running!"
+
+# =========================
+# RUN APP
+# =========================
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
