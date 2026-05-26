5ee1b7a019ccf21cb56697d304a7112742d248be
PAVANVSAI <pavanvsaimakkala12@gmail.com>
2026-05-26 16:48:59 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,8 +8,7 @@ GEMINI_API_KEY = "AIzaSyBKMwsVbs2vGeBUAEXcRau8UTSyX_4a7oI"
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+model = genai.GenerativeModel("gemini-2.0-flash")
 bot = Bot(token=BOT_TOKEN)
 
 app = Flask(__name__)
